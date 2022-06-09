e238a28af734c9f052cea4bc4fb5fb424b13e17d
Md. Osman Goni <67469321+osmangoni-web@users.noreply.github.com>
2022-06-09 13:57:32 +0600

    Add files via upload

---
+++ Correlation.m
@@ -0,0 +1,24 @@
+clc;
+clear all;
+close all;
+x=input("enter the 1st sequence");
+h=input("enter the 2nd sequence");
+y=crosscorr(x,h);
+figure;
+subplot(3,1,1);
+stem(x);
+ylabel("Amplitude --.");
+xlabel("(a) n --.");
+title("input sequence");
+subplot(3,1,2);
+stem(h);
+ylabel("Amplitude --.");
+xlabel("(b) n --.");
+title("impulse sequence");
+subplot(3,1,3);
+stem(fliplr(y));
+ylabel("Amplitude --.");
+xlabel("(c) n --.");
+title("Cross correlated sequence");
+disp("The resultant signal is");
+fliplr(y)
